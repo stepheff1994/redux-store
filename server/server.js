aa992d8e2e36777e7b7b9c6221b3cee0208337f5
Stephanie Freyler <stephaniefreylersantana@gmail.com>
2021-04-16 20:51:31 -0700

    fixed graphql error

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const express = require('./node_modules/@types/express');
-const { ApolloServer } = require('./node_modules/apollo-server-express/dist');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
